a63e0a26ae941f9d14108ea1690e0b0001af1ead
Heron Leal <heron_mx@hotmail.com>
2026-03-07 13:39:14 -0300

    Portfólio

---
+++ js/script.js.js
@@ -0,0 +1,48 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navibar.classList.toggle('active');
+}
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.array.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150;
+        let heigth = sec.offsetHeigth;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');   
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navibar.classList.remove('active');
+};
+
+ScrollReveal({
+     //reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 200
+     });
+
+     ScrollReveal().reveal('.home-content h3:first-of-type', {origin: 'top'})
+     ScrollReveal().reveal('.home-img,form', { origin: 'bottom' });
+     ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+     ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+   
